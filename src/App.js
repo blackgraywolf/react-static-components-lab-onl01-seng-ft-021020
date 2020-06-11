d3eb9bdb0e215433c14755fdd2c12cae431f2121
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-06-11 17:12:59 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
 	render() {
 		// your code in the return statement below!
+		
 		return (
 			<div className="App">
 				<CatComponent />
